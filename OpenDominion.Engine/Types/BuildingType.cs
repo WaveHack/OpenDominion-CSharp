57d06ce8f039a19310f49c2e92eced14e867d06b
WaveHack <email@wavehack.net>
2019-08-18 18:12:41 +0200

    Add BuildingType

---
+++ OpenDominion.Engine/Types/BuildingType.cs
@@ -0,0 +1,25 @@
+﻿namespace OpenDominion.Engine.Types
+{
+    public enum BuildingType
+    {
+        Home,
+        Alchemy,
+        Farm,
+        Smithy,
+        Masonry,
+        OreMine,
+        GryphonNest,
+        Tower,
+        WizardGuild,
+        Temple,
+        DiamondMine,
+        School,
+        Lumberyard,
+        ForestHaven,
+        Factory,
+        GuardTower,
+        Shrine,
+        Barracks,
+        Dock
+    }
+}
