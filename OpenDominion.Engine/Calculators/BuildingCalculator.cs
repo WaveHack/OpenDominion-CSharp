09ad2c5ca54742f501f658463abc717661b2d2e2
WaveHack <email@wavehack.net>
2019-08-18 21:14:23 +0200

    Add BuildingCalculator

---
+++ OpenDominion.Engine/Calculators/BuildingCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using OpenDominion.Engine.Models;
+
+namespace OpenDominion.Engine.Calculators
+{
+    public class BuildingCalculator
+    {
+        public int GetTotalBuildings(Dominion dominion)
+        {
+            return dominion.Buildings.Sum(pair => pair.Value);
+        }
+    }
+}
--- OpenDominion.Engine/Calculators/LandCalculator.cs
+++ OpenDominion.Engine/Calculators/LandCalculator.cs
@@ -1,10 +1,17 @@
-﻿using System.Linq;
+using System.Linq;
 using OpenDominion.Engine.Models;
 
 namespace OpenDominion.Engine.Calculators
 {
     public class LandCalculator
     {
+        private readonly BuildingCalculator _buildingCalculator;
+
+        public LandCalculator(BuildingCalculator buildingCalculator)
+        {
+            _buildingCalculator = buildingCalculator;
+        }
+
         public int GetTotalLand(Dominion dominion)
         {
             return dominion.Land.Sum(pair => pair.Value);
