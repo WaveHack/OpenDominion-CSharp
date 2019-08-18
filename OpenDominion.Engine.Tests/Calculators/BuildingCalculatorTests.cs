da4a1d06592ac4bc0c147f644dab646fa538dce4
WaveHack <email@wavehack.net>
2019-08-19 00:13:35 +0200

    Add BuildingCalculatorTests

---
+++ OpenDominion.Engine.Tests/Calculators/BuildingCalculatorTests.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using OpenDominion.Engine.Calculators;
+using OpenDominion.Engine.Models;
+using OpenDominion.Engine.Types;
+
+namespace OpenDominion.Engine.Tests.Calculators
+{
+    [TestFixture]
+    public class BuildingCalculatorTests
+    {
+        private BuildingCalculator _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new BuildingCalculator();
+        }
+
+        [Test]
+        public void GetTotalBuildings_Returns_TotalBuildings()
+        {
+            // Arrange
+            var dominion = new Dominion
+            {
+                Buildings =
+                {
+                    [BuildingType.Home] = 10,
+                    [BuildingType.Alchemy] = 30,
+                    [BuildingType.Farm] = 30,
+                    [BuildingType.Lumberyard] = 20
+                }
+            };
+
+            // Act
+            var totalBuildings = _sut.GetTotalBuildings(dominion);
+
+            // Assert
+            Assert.AreEqual(90, totalBuildings);
+        }
+    }
+}
